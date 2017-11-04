f3d982a3baf3b0b71d22539d5a7dcbc6db4a1b73
Ben Withington <withingb@aston.ac.uk>
2017-11-04 23:29:58 +0000

    Fire method now makes sure coordinates are inside the grid and that numbers are entered

--- Battleships/src/game/GameController.java
+++ Battleships/src/game/GameController.java
@@ -8,9 +8,9 @@ public class GameController {
 	private Player p1;
 	private Player p2;
 	private Scanner scanner;
-	
+
 	public GameController() {
-		
+
 		this.p1 = new Player("Player 1");
 		this.p2 = new Player("Player 2");
 		this.scanner = new Scanner(System.in);
@@ -19,62 +19,79 @@ public class GameController {
 		printBoard(p1, p2);
 		printBoard(p2, p1);
 
-		// TODO: Making sure all targeted locations are in bounds and that coordinates entered are numbers
-		
-		while(true) {
-			
+		while (true) {
+
 			int x = -1;
 			int y = -1;
-			
+
 			System.out.println("Player 1, Please Enter The Coordinates For Where You Would Like To Attack.");
-			System.out.println("X: ");
-			
-			if(scanner.hasNextLine()) {
+
+			do {
+				System.out.println("X: ");
+				
+				while(!scanner.hasNextInt()) {
+					System.out.println("Not A Number, Try Again.");
+					scanner.next();
+				}
 				x = scanner.nextInt();
-			}
-			
-			System.out.println("Y: ");
-			
-			if(scanner.hasNextLine()) {
+			} while (x < 0 || x > 9);
+
+			do {
+				System.out.println("Y: ");
+				
+				while(!scanner.hasNextInt()) {
+					System.out.println("Not A Number, Try Again.");
+					scanner.next();
+				}
 				y = scanner.nextInt();
-			}
-			
+			} while (y < 0 || y > 9);
+
 			p1.fireAt(p2, x, y);
-			
-			if(!p1.hasShips()) {
+
+			if (!p1.hasShips()) {
 				System.out.println("Congratulations Player 1, You've Sunken All Your Enemies Ships!");
 				break;
 			}
-			
+
 			///////////////////////////////////////////////////////////////////////////////////////////////////
-			
+
 			System.out.println("Player 2, Please Enter The Coordinates For Where You Would Like To Attack.");
 			System.out.println("X: ");
-			
-			if(scanner.hasNextLine()) {
+
+			do {
+				System.out.println("X: ");
+				
+				while(!scanner.hasNextInt()) {
+					System.out.println("Not A Number, Try Again.");
+					scanner.next();
+				}
 				x = scanner.nextInt();
-			}
-			
-			System.out.println("Y: ");
-			
-			if(scanner.hasNextLine()) {
+			} while (x < 0 || x > 9);
+
+			do {
+				System.out.println("Y: ");
+				
+				while(!scanner.hasNextInt()) {
+					System.out.println("Not A Number, Try Again.");
+					scanner.next();
+				}
 				y = scanner.nextInt();
-			}
-			
-			
+			} while (y < 0 || y > 9);
+
 			p2.fireAt(p1, x, y);
-			
-			if(!p2.hasShips()) {
+
+			if (!p2.hasShips()) {
 				System.out.println("Congratulations Player 2, You've Sunken All Your Enemies Ships!");
 				break;
 			}
-			
+
 			x = -1;
 			y = -1;
 		}
 	}
 
 	private void setUpShips() {
+		
 		// Ask Player 1 to set up their ships
 
 		System.out.println("========================");
@@ -86,6 +103,7 @@ public class GameController {
 		p1.addShip(3);
 		p1.addShip(3);
 		p1.addShip(2);
+		
 		// Ask Player 2 to set up their ships
 
 		System.out.println("========================");
@@ -97,15 +115,16 @@ public class GameController {
 		p2.addShip(3);
 		p2.addShip(3);
 		p2.addShip(2);
-		
+
 		System.out.println();
 	}
-	
-	
-	// TODO: Change printBoard so that it takes a player object and prints their perspective of the game
-	// ((Their gameboard with all ships showing and their opponents gameboard displaying HITS and MISSES
+
+	// TODO: Change printBoard so that it takes a player object and prints their
+	// perspective of the game
+	// ((Their gameboard with all ships showing and their opponents gameboard
+	// displaying HITS and MISSES
 	private void printBoard(Player currentPlayer, Player opposingPlayer) {
-	
+
 		System.out.println("=============================");
 		System.out.println(currentPlayer.getName() + "'s Board");
 		System.out.println("=============================");
@@ -128,7 +147,8 @@ public class GameController {
 		System.out.println("=============================");
 		System.out.println(opposingPlayer.getName() + "'s Board");
 		System.out.println("=============================");
-		// TODO: Make this so that the currentPlayer only knows where they've targeted and not where their opponents
+		// TODO: Make this so that the currentPlayer only knows where they've targeted
+		// and not where their opponents
 		// unknown ship locations are
 		for (int i = 0; i < 10; i++) {
 			for (int j = 0; j < 10; j++) {
