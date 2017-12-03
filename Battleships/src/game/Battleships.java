cd7d10ca5a4110341902207c61c786f557bb57d9
Ben Withington <withingb@aston.ac.uk>
2017-12-03 15:16:23 +0000

    removed a main method which was used for testing

--- Battleships/src/game/Battleships.java
+++ Battleships/src/game/Battleships.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 import game.Board.Cell;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -130,9 +131,5 @@ public class Battleships extends Application {
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
