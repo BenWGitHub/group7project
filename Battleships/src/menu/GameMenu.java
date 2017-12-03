5852a3cde3fe96d95b18d591e1f1cebb2b3cd7e5
Ben Withington <withingb@aston.ac.uk>
2017-12-03 15:41:17 +0000

    exit confirmation dialogue box

--- Battleships/src/menu/GameMenu.java
+++ Battleships/src/menu/GameMenu.java
@@ -7,10 +7,10 @@ import java.nio.file.Paths;
 import game.Battleships;
 import javafx.animation.FadeTransition;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.effect.GaussianBlur;
 import javafx.scene.effect.Glow;
@@ -20,13 +20,14 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-
+import javafx.scene.control.*;
 
 public class GameMenu extends Application {
 
@@ -52,24 +53,24 @@ public class GameMenu extends Application {
 
 		Scene scene = new Scene(root);
 
-		scene.setOnKeyPressed(event -> {
-
-			if (event.getCode() == KeyCode.ESCAPE) {
-				if (!gm.isVisible()) {
-					FadeTransition ft = new FadeTransition(Duration.seconds(0.5), gm);
-					ft.setFromValue(0);
-					ft.setToValue(1);
-					gm.setVisible(true);
-					ft.play();
-				} else {
-					FadeTransition ft = new FadeTransition(Duration.seconds(0.5), gm);
-					ft.setFromValue(1);
-					ft.setToValue(0);
-					ft.setOnFinished(evt -> gm.setVisible(false));
-					ft.play();
-				}
-			}
-		});
+		// scene.setOnKeyPressed(event -> {
+		//
+		// if (event.getCode() == KeyCode.ESCAPE) {
+		// if (!gm.isVisible()) {
+		// FadeTransition ft = new FadeTransition(Duration.seconds(0.5), gm);
+		// ft.setFromValue(0);
+		// ft.setToValue(1);
+		// gm.setVisible(true);
+		// ft.play();
+		// } else {
+		// FadeTransition ft = new FadeTransition(Duration.seconds(0.5), gm);
+		// ft.setFromValue(1);
+		// ft.setToValue(0);
+		// ft.setOnFinished(evt -> gm.setVisible(false));
+		// ft.play();
+		// }
+		// }
+		// });
 
 		primaryStage.setTitle("Battleships");
 		primaryStage.setScene(scene);
@@ -117,58 +118,84 @@ public class GameMenu extends Application {
 
 			MenuButton btnExit = new MenuButton(" EXIT");
 			btnExit.setOnMouseClicked(event -> {
-				System.exit(0);
+				Stage stage = new Stage();
+				
+				VBox layout = new VBox(10);
+				layout.setAlignment(Pos.CENTER);
+		    		Label question = new Label("Are You Sure You Want to Exit?");
+		    		
+		    		Button yesBtn = new Button("Yes");
+		    		yesBtn.setOnAction(yesEvent ->{
+		    			Platform.exit();
+		    		});
+		    		
+		    		Button noBtn = new Button("No");
+		    		
+		    		noBtn.setOnAction(noEvent ->{
+		    			stage.close();
+		    		});
+		    		HBox buttonBox = new HBox(10);
+		    		buttonBox.setAlignment(Pos.CENTER);
+		    		
+		    		
+		    		buttonBox.getChildren().addAll(yesBtn, noBtn);
+		    		layout.getChildren().addAll(question, buttonBox);
+		    		
+		    		Scene resultScene = new Scene(layout, 200, 100);
+		    		
+		    		stage.setTitle("Exit");
+		    		stage.setScene(resultScene);
+		    		stage.show();
+			
 			});
 
+		menu0.getChildren().addAll(btnSinglePlayer,btnMultiPlayer,btnExit);
 
-			menu0.getChildren().addAll(btnSinglePlayer, btnMultiPlayer, btnExit);
+		Rectangle bg = new Rectangle(800, 600);bg.setFill(Color.GRAY);bg.setOpacity(0.35);
 
-			Rectangle bg = new Rectangle(800, 600);
-			bg.setFill(Color.GRAY);
-			bg.setOpacity(0.35);
-
-			this.getChildren().addAll(bg, menu0);
-		}
-
+		this.getChildren().addAll(bg,menu0);
 	}
 
-	private static class MenuButton extends StackPane {
-		private Text text;
+}
 
-		public MenuButton(String name) {
-			text = new Text(name);
-			text.setFont(Font.font(20));
-			text.setFill(Color.WHITE);
+private static class MenuButton extends StackPane {
+	private Text text;
 
-			Rectangle bg = new Rectangle(250, 30);
-			bg.setOpacity(0.6);
+	public MenuButton(String name) {
+		text = new Text(name);
+		text.setFont(Font.font(20));
+		text.setFill(Color.WHITE);
+
+		Rectangle bg = new Rectangle(250, 30);
+		bg.setOpacity(0.6);
+		bg.setFill(Color.BLACK);
+		bg.setEffect(new GaussianBlur(3.5));
+
+		this.setAlignment(Pos.CENTER_LEFT);
+		this.setRotate(-0.5);
+		this.getChildren().addAll(bg, text);
+
+		this.setOnMouseEntered(event -> {
+			bg.setTranslateX(10);
+			text.setTranslateX(10);
+			bg.setFill(Color.WHITE);
+			text.setFill(Color.BLACK);
+		});
+
+		this.setOnMouseExited(event -> {
+			bg.setTranslateX(0);
+			text.setTranslateX(0);
 			bg.setFill(Color.BLACK);
-			bg.setEffect(new GaussianBlur(3.5));
+			text.setFill(Color.WHITE);
+		});
 
-			this.setAlignment(Pos.CENTER_LEFT);
-			this.setRotate(-0.5);
-			this.getChildren().addAll(bg, text);
+		DropShadow drop = new DropShadow(50, Color.WHITE);
+		drop.setInput(new Glow());
 
-			this.setOnMouseEntered(event -> {
-				bg.setTranslateX(10);
-				text.setTranslateX(10);
-				bg.setFill(Color.WHITE);
-				text.setFill(Color.BLACK);
-			});
+		this.setOnMousePressed(event -> setEffect(drop));
+		this.setOnMouseReleased(event -> setEffect(null));
+	}
 
-			this.setOnMouseExited(event -> {
-				bg.setTranslateX(0);
-				text.setTranslateX(0);
-				bg.setFill(Color.BLACK);
-				text.setFill(Color.WHITE);
-			});
-
-			DropShadow drop = new DropShadow(50, Color.WHITE);
-			drop.setInput(new Glow());
-
-			this.setOnMousePressed(event -> setEffect(drop));
-			this.setOnMouseReleased(event -> setEffect(null));
-		}
 	}
 
 	public static void main(String args[]) {
